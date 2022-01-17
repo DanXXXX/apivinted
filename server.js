94e2224e563feaf6c4e3a739a947d1503e143b23
Victoire Nyoung <nyoungvictoire@gmail.com>
2022-01-17 17:33:55 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+
+
+const mongoose = require('mongoose');
+require('dotenv').config({path: './config/.env'});
+require('./config/db');
+
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const server = express();
+
+server.use(logger('dev'));
+server.use(express.json());
+server.use(express.urlencoded({ extended: false }));
+server.use(cookieParser());
+server.use(express.static(path.join(__dirname, 'public')));
+
+server.use('/', indexRouter);
+server.use('/users', usersRouter);
+
+module.exports = server;
